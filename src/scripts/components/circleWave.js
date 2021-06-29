bd51b5db23683bee29913cb920d67f4cf72884fc
kondo tomonori <tomonorik1001@gmail.com>
2021-06-29 23:24:56 +0900

    🎉

--- src/scripts/components/circleWave.js
+++ src/scripts/components/circleWave.js
@@ -13,13 +13,23 @@ export const circleWave = () => {
       p.noFill();
       p.stroke('#fff')
 
-      for(let i = 0; i < 30; i ++){
+      for(let i = 0; i < 20; i ++){
+
+        // 色ランダム
+        const r = p.map(p.sin(p.frameCount),-1,1,0,255);
+        const g = p.map(i,0,30,0,255);
+        const b = p.map(p.cos(p.frameCount),-1,1,200,255);
+
+        p.stroke(0,0,b);
+
+        // p.rotate(p.frameCount / 100);
+
         p.beginShape();
-        for(let j = 0; j < 360; j += 10){
-          const rad = i * 10;
+        for(let j = 0; j < 360; j += 40){
+          const rad = i * 20;
           const x = rad * p.cos(j);
           const y = rad * p.sin(j);
-          const z = p.sin(p.frameCount * 2 + i * 10) * 50;
+          const z = p.sin(p.frameCount * 2 + i * 5) * 200;
 
           p.vertex(x, y, z)
         }
